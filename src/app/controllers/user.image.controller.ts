4c09cf4ad7e1063269e2b22cb297dc243e67da69
Harry Parkes <harrydparkes@proton.me>
2024-03-06 20:53:44 +1300

    preparing for image functionality

--- src/app/controllers/user.image.controller.ts
+++ src/app/controllers/user.image.controller.ts
@@ -1,46 +1,44 @@
 import {Request, Response} from "express";
 
 import Logger from "../../config/logger";
+import {deleteUserImage, getUserImage, uploadUserImage} from "../models/user.image.model";
 
+
+async function processRequest(
+    request: Request,
+    callback: (body: number) => Promise<[number, string, object | void]>
+): Promise<[number, string, object | void]> {
+    try {
+        const id = parseInt(request.params.id, 10);
+        if (!isNaN(id)) {
+            return await callback(id);
+        } else {
+            return [400, `Bad Request: Malformed userId`, void 0];
+        }
+    } catch (err) {
+        return [500, "Internal Server Error", void 0];
+    }
+}
+
+async function respond(
+    request: Request,
+    response: Response,
+    callback: (body: number) => Promise<[number, string, object | void]>
+): Promise<void> {
+    const [status, message, result] = await processRequest(request, callback);
+    Logger.log(status >= 300 ? 'error': 'info', message);
+    response.statusMessage = message;
+    response.status(status).send(result);
+}
 
 export async function getImage(request: Request, response: Response): Promise<void> {
-    try {
-        // Your code goes here
-        response.statusMessage = "Not Implemented Yet!";
-        response.status(501).send();
-        return;
-    } catch (err) {
-        Logger.error(err);
-        response.statusMessage = "Internal Server Error";
-        response.status(500).send();
-        return;
-    }
+    await respond(request, response, getUserImage);
 }
 
 export async function setImage(request: Request, response: Response): Promise<void> {
-    try {
-        // Your code goes here
-        response.statusMessage = "Not Implemented Yet!";
-        response.status(501).send();
-        return;
-    } catch (err) {
-        Logger.error(err);
-        response.statusMessage = "Internal Server Error";
-        response.status(500).send();
-        return;
-    }
+    await respond(request, response, uploadUserImage);
 }
 
 export async function deleteImage(request: Request, response: Response): Promise<void> {
-    try {
-        // Your code goes here
-        response.statusMessage = "Not Implemented Yet!";
-        response.status(501).send();
-        return;
-    } catch (err) {
-        Logger.error(err);
-        response.statusMessage = "Internal Server Error";
-        response.status(500).send();
-        return;
-    }
+    await respond(request, response, deleteUserImage);
 }
---
+++ src/app/models/user.image.model.ts
@@ -0,0 +1,10 @@
+export async function getUserImage(userId: number): Promise<[number, string, object | void]> {
+    return [501, "Not Implemented Yet!", void 0];
+}
+
+export async function uploadUserImage(userId: number): Promise<[number, string, object | void]> {
+    return [501, "Not Implemented Yet!", void 0];
+}
+export async function deleteUserImage(userId: number): Promise<[number, string, object | void]> {
+    return [501, "Not Implemented Yet!", void 0];
+}
