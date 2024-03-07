81b24132371c8112ea126b26bc24e1066c6c4c4b
Harry Parkes <harrydparkes@proton.me>
2024-03-07 21:44:29 +1300

    saving images into storage!

--- src/app/controllers/user.image.controller.ts
+++ src/app/controllers/user.image.controller.ts
@@ -1,4 +1,6 @@
 import {Request, Response} from "express";
+import path from 'path';
+import fs from 'fs';
 
 import Logger from "../../config/logger";
 import {deleteUserImage, getUserImage, uploadUserImage} from "../models/user.image.model";
@@ -31,12 +33,32 @@ async function respond(
     response.status(status).send(result);
 }
 
+function contentType(request: Request): string {
+    return request.headers['content-type'] || '' as string;
+}
+
 export async function getImage(request: Request, response: Response): Promise<void> {
     await respond(request, response, getUserImage);
 }
 
 export async function setImage(request: Request, response: Response): Promise<void> {
-    await respond(request, response, uploadUserImage);
+    const imageBuffer: Buffer = request.body;
+    Logger.info(`Received image of size ${imageBuffer.length}`);
+    const fileExtension = contentType(request).split('/')[1];
+
+    // Define the path where the image will be saved
+    const imagePath = path.join(__dirname, '../../../storage/images/' + request.params.id + '.' + fileExtension);
+
+    // Write the image data to a file
+    fs.writeFile(imagePath, imageBuffer, (err) => {
+        if (err) {
+            Logger.error(`Error saving image: ${err.message}`);
+            response.status(500).send("Internal Server Error");
+        } else {
+            Logger.info(`Image saved successfully at ${imagePath}`);
+            response.status(200).send("Image saved successfully");
+        }
+    });
 }
 
 export async function deleteImage(request: Request, response: Response): Promise<void> {
