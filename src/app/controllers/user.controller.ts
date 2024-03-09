f7acfddfcc79599f7f14eda32f2c0644e9bbc0e1
Harrison Parkes <hpa101@uclive.ac.nz>
2024-03-10 09:15:29 +1300

    moved common logic from individual controllers to new shared common controller

--- src/app/controllers/user.controller.ts
+++ src/app/controllers/user.controller.ts
@@ -1,42 +1,9 @@
 import {Request, Response} from "express";
-import addFormats from 'ajv-formats';
-import Ajv from 'ajv';
 
 import * as schemas from '../resources/schemas.json';
 import * as users from '../models/user.model';
 import Logger from '../../config/logger';
 
-
-const ajv = new Ajv({removeAdditional: 'all'});
-addFormats(ajv);
-
-function authorisation(request: Request): string {
-    return request.headers["x-authorization"] as string;
-}
-
-async function processRequestBody<Input>(
-    request: Request,
-    response: Response,
-    schema: object,
-    callback: (body: Input) => Promise<[number, string, object | void]>
-): Promise<void> {
-    try {
-        const validator = ajv.compile<Input>(schema);
-        if (!validator(request.body)) {
-            response.statusMessage = `Bad Request: ${ajv.errorsText(validator.errors)}`;
-            response.status(400).send();
-        } else {
-            const [status, message, result] = await callback(request.body);
-            Logger.info(message);
-            response.statusMessage = message;
-            response.status(status).send(result);
-        }
-    } catch (err) {
-        Logger.error(err);
-        response.statusMessage = "Internal Server Error";
-        response.status(500).send();
-    }
-}
 
 export async function register(request: Request, response: Response): Promise<void> {
     await processRequestBody(request, response, schemas.user_register, users.registerUser);
