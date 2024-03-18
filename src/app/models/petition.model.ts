9ea62e581bb6ba3f08900119bd545c4823ffe020
Harrison Parkes <hpa101@uclive.ac.nz>
2024-03-19 10:50:42 +1300

    I think I implemented the petition query function

--- src/app/models/petition.model.ts
+++ src/app/models/petition.model.ts
@@ -6,7 +6,88 @@ import snakeToCamel from "../services/snakeToCamelConverter";
 import {runSQL} from "../../config/db";
 
 export async function allPetitions(body: PetitionSearch): Promise<[number, string, object | void]> {
-    return [501, "not implemented", void 0];
+    const whereClause: string[] = [];
+    if (body.ownerId) {
+        whereClause.push(`owner.id = '${body.ownerId}'`);
+    }
+    if (body.q) {
+        whereClause.push(`(petition.title LIKE '%${body.q}%' OR petition.description LIKE '%${body.q}%')`);
+    }
+    if (body.categoryIds) {
+        if (Array.isArray(body.categoryIds)) {
+            whereClause.push(`petition.category_id IN (${body.categoryIds.join(", ")})`);
+        } else {
+            whereClause.push(`petition.category_id = ${body.categoryIds}`);
+        }
+    }
+    const havingClause: string[] = [];
+    if (body.supportingCost) {
+        havingClause.push(`MIN(support_tier.cost) <= ${body.supportingCost}`);
+    }
+    if (body.supporterId) {
+        havingClause.push(`SUM(CASE WHEN supporter.id = ${body.supporterId} THEN 1 ELSE 0 END) > 0;`);
+    }
+    let orderByClause = "";
+    if (body.sortBy) {
+        switch (body.sortBy) {
+            case "ALPHABETICAL_ASC":
+                orderByClause = "petition.title ASC";
+                break;
+            case "ALPHABETICAL_DESC":
+                orderByClause = "petition.title DESC";
+                break;
+            case "COST_ASC":
+                orderByClause = "MIN(support_tier.cost) ASC";
+                break;
+            case "COST_DESC":
+                orderByClause = "MIN(support_tier.cost) DESC";
+                break;
+            case "CREATED_ASC":
+                orderByClause = "petition.creation_date ASC";
+                break;
+            case "CREATED_DESC":
+                orderByClause = "petition.creation_date DESC";
+                break;
+        }
+    }
+    if (orderByClause.length > 0) {
+        orderByClause = `ORDER BY ${orderByClause}, petition.id ASC`;
+    }
+    const petitions = await runSQL<Petition[]>(
+        `SELECT petition.description,
+                SUM(support_tier.cost)            AS money_raised,
+                petition.id                       AS petition_id,
+                petition.title                    AS title,
+                petition.category_id              AS category_id,
+                petition.owner_id                 AS owner_id,
+                owner.first_name                  AS owner_first_name,
+                owner.last_name                   AS owner_last_name,
+                COUNT(DISTINCT supporter.user_id) AS number_of_supporters,
+                petition.creation_date            AS creation_date
+         FROM petition
+                  JOIN user AS owner ON owner.id = petition.owner_id
+                  JOIN supporter ON supporter.petition_id = petition.id
+                  JOIN support_tier ON support_tier.id = supporter.support_tier_id
+         ${whereClause.length > 0 ? `WHERE ${whereClause.join("AND ")}` : ""}
+         GROUP BY petition.id
+         ${havingClause.length > 0 ? `HAVING ${havingClause.join("AND ")}` : ""}
+         ${orderByClause};`
+    );
+    if (body.startIndex) {
+        petitions.splice(parseInt(body.startIndex, 10), petitions.length);
+    }
+    if (body.count) {
+        petitions.splice(0, parseInt(body.count, 10));
+    }
+    const camelCasePetitions = snakeToCamel(petitions);
+    for (const petition of camelCasePetitions) {
+        petition.supportTiers = snakeToCamel(await runSQL<SupportTier[]>(
+            `SELECT id AS support_tier_id, title, description, cost
+             FROM support_tier
+             WHERE petition_id = ${petition.petitionId};`
+        ));
+    }
+    return [200, "All petitions found", camelCasePetitions];
 }
 
 export async function singlePetition(petitionId: number): Promise<[number, string, object | void]> {
