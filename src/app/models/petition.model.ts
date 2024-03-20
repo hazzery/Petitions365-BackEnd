aab830610cc994a0b9a6e736218338e9d926a08d
Harrison Parkes <hpa101@uclive.ac.nz>
2024-03-20 15:33:03 +1300

    completed petition querying logic

--- src/app/models/petition.model.ts
+++ src/app/models/petition.model.ts
@@ -1,9 +1,10 @@
 import {ResultSetHeader} from "mysql2";
 
 import {PetitionPatch, PetitionPost, PetitionSearch} from "../types/requestBodySchemaInterfaces";
-import {Petition, SupportTier} from "../types/databaseRowDataPackets";
+import {DetailedPetition, PetitionOverview, SupportTier} from "../types/databaseRowDataPackets";
 import snakeToCamel from "../services/snakeToCamelConverter";
 import {runSQL} from "../../config/db";
+
 
 export async function allPetitions(body: PetitionSearch): Promise<[number, string, object | void]> {
     const whereClause: string[] = [];
@@ -25,37 +26,34 @@ export async function allPetitions(body: PetitionSearch): Promise<[number, strin
         havingClause.push(`MIN(support_tier.cost) <= ${body.supportingCost}`);
     }
     if (body.supporterId) {
-        havingClause.push(`SUM(CASE WHEN supporter.id = ${body.supporterId} THEN 1 ELSE 0 END) > 0;`);
+        havingClause.push(`SUM(CASE WHEN supporter.user_id = ${body.supporterId} THEN 1 ELSE 0 END) > 0`);
     }
-    let orderByClause = "";
-    if (body.sortBy) {
-        switch (body.sortBy) {
-            case "ALPHABETICAL_ASC":
-                orderByClause = "petition.title ASC";
-                break;
-            case "ALPHABETICAL_DESC":
-                orderByClause = "petition.title DESC";
-                break;
-            case "COST_ASC":
-                orderByClause = "MIN(support_tier.cost) ASC";
-                break;
-            case "COST_DESC":
-                orderByClause = "MIN(support_tier.cost) DESC";
-                break;
-            case "CREATED_ASC":
-                orderByClause = "petition.creation_date ASC";
-                break;
-            case "CREATED_DESC":
-                orderByClause = "petition.creation_date DESC";
-                break;
-        }
+    let orderByClause: string;
+    switch (body.sortBy) {
+        case "ALPHABETICAL_ASC":
+            orderByClause = "petition.title ASC,";
+            break;
+        case "ALPHABETICAL_DESC":
+            orderByClause = "petition.title DESC,";
+            break;
+        case "COST_ASC":
+            orderByClause = "MIN(support_tier.cost) ASC,";
+            break;
+        case "COST_DESC":
+            orderByClause = "MIN(support_tier.cost) DESC,";
+            break;
+        case "CREATED_ASC":
+            orderByClause = "petition.creation_date ASC,";
+            break;
+        case "CREATED_DESC":
+            orderByClause = "petition.creation_date DESC,";
+            break;
+        default:
+            orderByClause = "petition.creation_date ASC,";
+            break;
     }
-    if (orderByClause.length > 0) {
-        orderByClause = `ORDER BY ${orderByClause}, petition.id ASC`;
-    }
-    const petitions = await runSQL<Petition[]>(
-        `SELECT petition.description,
-                SUM(support_tier.cost)            AS money_raised,
+    const petitions = await runSQL<PetitionOverview[]>(
+        `SELECT MIN(support_tier.cost)            AS supporting_cost,
                 petition.id                       AS petition_id,
                 petition.title                    AS title,
                 petition.category_id              AS category_id,
@@ -67,32 +65,25 @@ export async function allPetitions(body: PetitionSearch): Promise<[number, strin
          FROM petition
                   JOIN user AS owner ON owner.id = petition.owner_id
                   JOIN supporter ON supporter.petition_id = petition.id
-                  JOIN support_tier ON support_tier.id = supporter.support_tier_id
-         ${whereClause.length > 0 ? `WHERE ${whereClause.join("AND ")}` : ""}
-         GROUP BY petition.id
-         ${havingClause.length > 0 ? `HAVING ${havingClause.join("AND ")}` : ""}
-         ${orderByClause};`
+                  JOIN support_tier ON support_tier.petition_id = petition.id
+             ${whereClause.length > 0 ? `WHERE ${whereClause.join("AND ")}` : ""}
+         GROUP BY petition.id ${havingClause.length > 0 ? `HAVING ${havingClause.join("AND ")}` : ""}
+         ORDER BY ${orderByClause} petition.id ASC;`
     );
+    const count = petitions.length;
     if (body.startIndex) {
-        petitions.splice(parseInt(body.startIndex, 10), petitions.length);
+        petitions.splice(0, parseInt(body.startIndex, 10));
     }
     if (body.count) {
-        petitions.splice(0, parseInt(body.count, 10));
+        petitions.splice(parseInt(body.count, 10), petitions.length);
     }
     const camelCasePetitions = snakeToCamel(petitions);
-    for (const petition of camelCasePetitions) {
-        petition.supportTiers = snakeToCamel(await runSQL<SupportTier[]>(
-            `SELECT id AS support_tier_id, title, description, cost
-             FROM support_tier
-             WHERE petition_id = ${petition.petitionId};`
-        ));
-    }
-    return [200, "All petitions found", camelCasePetitions];
+    return [200, "All petitions matching given query have been sent", {petitions: camelCasePetitions, count}];
 }
 
 export async function singlePetition(petitionId: number): Promise<[number, string, object | void]> {
     try {
-        const [petition] = await runSQL<Petition[]>(
+        const [petition] = await runSQL<DetailedPetition[]>(
             `SELECT petition.description,
                     SUM(support_tier.cost)            AS money_raised,
                     petition.id                       AS petition_id,
--- src/app/types/databaseRowDataPackets.ts
+++ src/app/types/databaseRowDataPackets.ts
@@ -7,7 +7,19 @@ export interface SupportTier extends RowDataPacket {
     cost: number
 }
 
-export interface Petition extends RowDataPacket {
+export interface PetitionOverview extends RowDataPacket {
+    supporting_cost: number;
+    petition_id: number;
+    title: string;
+    category_id: number;
+    owner_id: number;
+    owner_first_name: string;
+    owner_last_name: string;
+    number_of_supporters: number;
+    creation_date: string;
+}
+
+export interface DetailedPetition extends RowDataPacket {
     description: string;
     money_raised: number;
     petition_id: number;
