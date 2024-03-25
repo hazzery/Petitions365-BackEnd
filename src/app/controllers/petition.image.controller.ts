843136f808d64f0176e8b66e52a66c018e179839
Harrison Parkes <hpa101@uclive.ac.nz>
2024-03-25 14:43:42 +1300

    implemented petition images

--- src/app/controllers/petition.image.controller.ts
+++ src/app/controllers/petition.image.controller.ts
@@ -1,11 +1,18 @@
 import {Request, Response} from "express";
 
-import {uploadUserImage} from "../models/user.image.model";
-import {contentType, respond} from "./common.controller";
+import {authenticationToken, contentType, respond, respondImage} from "./common.controller";
+import {getPetitionImage, uploadPetitionImage} from "../models/petition.image.model";
+import {getUserId} from "../services/sessions";
 
 
 export async function getImage(request: Request, response: Response): Promise<void> {
-
+    const petitionId = parseInt(request.params.id, 10);
+    if (isNaN(petitionId)) {
+        response.status(400).send("Invalid petition id");
+        return;
+    }
+    const callback = () => getPetitionImage(petitionId);
+    await respondImage(response, callback);
 }
 
 export async function setImage(request: Request, response: Response): Promise<void> {
@@ -14,11 +21,21 @@ export async function setImage(request: Request, response: Response): Promise<vo
         response.status(400).send("Invalid petition id");
         return;
     }
+    const token = authenticationToken(request);
+    if (token === undefined) {
+        response.status(401).send("Unauthenticated: No x-authorization header");
+        return;
+    }
+    const userId = await getUserId(token)
+    if (userId === undefined) {
+        response.status(401).send("Unauthenticated: Invalid token");
+        return;
+    }
     const fileExtension = contentType(request).split('/')[1];
     if (fileExtension === undefined || !['png', 'jpeg', 'gif'].includes(fileExtension)) {
         response.status(400).send("Invalid content type, must be one of: png, jpeg, gif");
         return;
     }
-    const callback = () => uploadUserImage(request.body as Buffer, petitionId, fileExtension);
+    const callback = () => uploadPetitionImage(request.body as Buffer, petitionId, fileExtension, userId);
     await respond(response, callback);
 }
---
+++ src/app/models/petition.image.model.ts
@@ -0,0 +1,70 @@
+import {runPreparedSQL} from "../../config/db";
+import {RowDataPacket} from "mysql2";
+import path from "path";
+import fs from "fs";
+
+
+const imageDirectory = path.join(__dirname, "../../../storage/images");
+
+
+/**
+ * Check if a petition has a hero image.
+ * @param petitionId - The id of the petition to check
+ * @returns The filename of the petition's hero image,
+ *  or null if the petition does not have a hero image,
+ *  or undefined if the petition does not exist.
+ */
+async function checkPetitionImage(petitionId: number): Promise<string | null | undefined> {
+    interface PetitionImage extends RowDataPacket {
+        image_filename: string | null;
+    }
+
+    const [petition] = await runPreparedSQL<PetitionImage[]>(
+        `SELECT image_filename
+         FROM petition
+         WHERE id = ?`,
+        [petitionId]
+    );
+    return petition?.image_filename;
+}
+
+export async function uploadPetitionImage(
+    image: Buffer,
+    petitionId: number,
+    fileExtension: string,
+    userId: number
+): Promise<[number, string, object | void]> {
+    const existingFileName = await checkPetitionImage(petitionId);
+    if (existingFileName === undefined) {
+        return [404, `No petition ${petitionId}`, void 0];
+    }
+    const fileName = `${userId}.${fileExtension}`;
+    const newFilePath = path.join(imageDirectory, fileName);
+    try {
+        await fs.promises.writeFile(newFilePath, image);
+        await runPreparedSQL(
+            `UPDATE petition
+             SET image_filename = ?
+             WHERE id = ?`,
+            [fileName, petitionId]
+        );
+        if (existingFileName !== null) {
+            return [200, `Image updated successfully`, void 0];
+        } else {
+            return [201, "Image saved successfully", void 0];
+        }
+    } catch (error) {
+        return [500, `Error saving image: ${error.message}`, void 0];
+    }
+}
+
+export async function getPetitionImage(petitionId: number): Promise<[number, string, object | void, string]> {
+    const fileName = await checkPetitionImage(petitionId);
+    if (!fileName) {
+        return [404, `Petition ${petitionId} does not have a hero image`, void 0, ""];
+    }
+    const filePath = path.join(imageDirectory, fileName);
+    const image = await fs.promises.readFile(filePath);
+    const mimeType = 'image/' + fileName.split('.')[1];
+    return [200, `Found image for petition ${petitionId}`, image, mimeType];
+}
