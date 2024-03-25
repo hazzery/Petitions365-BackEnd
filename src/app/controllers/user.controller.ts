58fa09d078c024f7e6d24465500c116889dd6f08
Harrison Parkes <hpa101@uclive.ac.nz>
2024-03-26 10:56:29 +1300

    using authoriseRequest and respondErrors in user controller

--- src/app/controllers/user.controller.ts
+++ src/app/controllers/user.controller.ts
@@ -2,7 +2,7 @@ import {Request, Response} from "express";
 import addFormats from 'ajv-formats';
 import Ajv from 'ajv';
 
-import {authenticationToken, processRequestBody, respond} from './common.controller';
+import {authenticationToken, authoriseRequest, processRequestBody, respond, respondErrors} from './common.controller';
 import {UserEdit} from "../types/requestBodySchemaInterfaces";
 import * as schemas from '../resources/schemas.json';
 import * as users from '../models/user.model';
@@ -58,21 +58,10 @@ export async function view(request: Request, response: Response): Promise<void> 
 }
 
 export async function update(request: Request, response: Response): Promise<void> {
-    const token = authenticationToken(request)
-    if (!token) {
-        Logger.warn("Bad Request: No authorization token");
-        response.statusMessage = "Unauthenticated: No authorization token";
-        response.status(401).send();
-        return;
-    }
-    const userId = parseInt(request.params.id, 10);
-    if (isNaN(userId) || userId <= 0) {
-        Logger.warn("Bad Request: Invalid user ID");
-        response.statusMessage = "Bad Request: Invalid user ID";
-        response.status(400).send();
-        return;
-    }
-    const editUser = async (body: UserEdit) => await users.updateUser(userId, token, body);
+    const authorise = async () => await authoriseRequest(authenticationToken(request), request.params.id);
+    const userId = await respondErrors(response, authorise);
+    if (userId === undefined) return;
+    const editUser = async (body: UserEdit) => await users.updateUser(userId, body);
     const callback = async () => await processRequestBody(request.body, schemas.user_edit, editUser);
     await respond(response, callback);
 }
