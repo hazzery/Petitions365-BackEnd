434d8af019288931d26485554e1b7fe70e832d91
Harrison Parkes <hpa101@uclive.ac.nz>
2024-03-26 10:57:15 +1300

    using authoriseRequest and respondErrors in user image controller

--- src/app/controllers/user.image.controller.ts
+++ src/app/controllers/user.image.controller.ts
@@ -1,8 +1,14 @@
 import {Request, Response} from "express";
 
-import {authenticationToken, contentType, respond, respondImage} from "./common.controller";
+import {
+    authenticationToken,
+    authoriseRequest,
+    contentType,
+    respond,
+    respondErrors,
+    respondImage
+} from "./common.controller";
 import {deleteUserImage, getUserImage, uploadUserImage} from "../models/user.image.model";
-import {getUserId} from "../services/sessions";
 
 
 export async function getImage(request: Request, response: Response): Promise<void> {
@@ -16,11 +22,9 @@ export async function getImage(request: Request, response: Response): Promise<vo
 }
 
 export async function setImage(request: Request, response: Response): Promise<void> {
-    const userId = parseInt(request.params.id, 10);
-    if (isNaN(userId)) {
-        response.status(400).send("Invalid user id");
-        return;
-    }
+    const authorise = async () => await authoriseRequest(authenticationToken(request), request.params.id);
+    const userId = await respondErrors(response, authorise);
+    if (userId === undefined) return;
     const fileExtension = contentType(request).split('/')[1];
     if (fileExtension === undefined || !['png', 'jpeg', 'gif'].includes(fileExtension)) {
         response.status(400).send("Invalid content type, must be one of: png, jpeg, gif");
@@ -31,20 +35,9 @@ export async function setImage(request: Request, response: Response): Promise<vo
 }
 
 export async function deleteImage(request: Request, response: Response): Promise<void> {
-    const userId = parseInt(request.params.id, 10);
-    if (isNaN(userId)) {
-        response.status(400).send("Invalid user id");
-        return;
-    }
-    const token = authenticationToken(request);
-    if (token === undefined) {
-        response.status(401).send("Unauthenticated: No x-authorization header");
-        return;
-    }
-    if (userId !== await getUserId(token)) {
-        response.status(403).send("Forbidden: You may not delete another user's image");
-        return;
-    }
+    const authorise = async () => await authoriseRequest(authenticationToken(request), request.params.id);
+    const userId = await respondErrors(response, authorise);
+    if (userId === undefined) return;
     const callback = () => deleteUserImage(userId);
     await respond(response, callback);
 }
